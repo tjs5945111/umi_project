781a4fd62738df6905a1b21838b5d82bfa1b5557
童建设 <tongjianshe@B-51MLQ05P-2334.local>
2021-12-14 19:36:39 +0800

    feat:组件开发

---
+++ myapp/src/components/BaseTable/index.tsx
@@ -0,0 +1,295 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {
+    Card,
+    Form,
+    Table,
+    Row,
+    Col,
+    Button,
+    Input,
+    Select,
+    Checkbox,
+} from 'antd';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+// import styles from './index.less';
+const { Option } = Select;
+
+type on = (x?: any, y?: any) => void;
+
+export interface BaseTableState { }
+
+export interface BaseTablehProps {
+    showHeader?: boolean;
+    loading?: boolean;
+    hideSearch?: boolean;
+    hideState?: boolean;
+    showSelection?: boolean; // 可选择
+    dataSource: any[];
+    columns: any[];
+    searchArray?: any[]; // 搜索表达每一项{name:'',value:''}
+    handleSelected?: on;
+    handlePaging?: on; // 分页处理事件
+    tableName?: string;
+    actionList?: any[]; // 表格操作按钮列表
+    radioList?: any[]; // 表格操作按钮列表
+    handleSearch?: on; // 搜索处理事件
+    pagingSizeChange?: on; // 每页条数变化事件
+    totalSize?: number; // 总数
+}
+
+const checkBoxOptions = [
+    { label: '创建', value: 'DRAFT&UPDATED' },
+    { label: '预发', value: 'PRE_RELEASED' },
+    { label: '审批中', value: 'PROCESSING' },
+    {
+        label: '灰度',
+        value: 'WHITE_LIST_GRAY&INNER_GRAY&EXTERNAL_GRAY&WHITE_LIST_GRAY_D&EXTERNAL_GRAY_D',
+    },
+    { label: '发布', value: 'RELEASED' },
+    {
+        label: '下线',
+        value: 'WHITE_LIST_GRAY_D&PROCESSING_D&EXTERNAL_GRAY_D&INVALID',
+    },
+];
+
+const ALL_SEARCH_STATUS = [
+    'DRAFT&UPDATED',
+    'PRE_RELEASED',
+    'PROCESSING',
+    'WHITE_LIST_GRAY&INNER_GRAY&EXTERNAL_GRAY&WHITE_LIST_GRAY_D&EXTERNAL_GRAY_D',
+    'RELEASED',
+    'WHITE_LIST_GRAY_D&PROCESSING_D&EXTERNAL_GRAY_D&INVALID',
+];
+
+export default function BaseTable({
+    showHeader = true,
+    dataSource,
+    loading,
+    columns,
+    handleSelected,
+    handlePaging,
+    hideSearch = false,
+    searchArray,
+    tableName,
+    actionList,
+    radioList,
+    showSelection = false,
+    handleSearch,
+    pagingSizeChange,
+    totalSize,
+    hideState = false,
+}: BaseTablehProps) {
+    const [selectedRowKeys, SetSelectedRowKeys] = useState([]);
+    const [expand, SetExpand] = useState(false);
+    const [formatSearchStatus, SetFormatSearchStatus] = useState<any>([]);
+    const [checkAll, SetcheckAll] = useState<any>(false);
+    const [searchParams, SetSearchParams] = useState({});
+    const formEl = useRef(null);
+    useEffect(() => {
+        setTimeout(() => {
+            if (!formEl || !formEl?.current) return;
+            const { setFieldsValue } = formEl.current;
+            Object.keys(searchParams || {}).length && setFieldsValue(searchParams);
+        }, 0);
+    }, [dataSource]);
+    const handleSelectedChange = (rowKeys, rows) => {
+        SetSelectedRowKeys(rowKeys);
+        typeof handleSelected !== 'undefined' && handleSelected(rowKeys, rows);
+        // console.log(rowKeys, rows);
+    };
+
+    const pagingChange = (page: any) => {
+        typeof handlePaging !== 'undefined' && handlePaging(page);
+    };
+
+    const onCheckAllChange = e => {
+        SetFormatSearchStatus(e.target.checked ? ALL_SEARCH_STATUS : []);
+        SetcheckAll(e.target.checked);
+    };
+
+    const getFields = (form: any) => {
+        if (!searchArray) {
+            return [];
+        }
+        const count = expand ? searchArray?.length : 6;
+        const children: any = [];
+        searchArray?.forEach((ele, index) => {
+            children.push(
+                <Col span={8} key={index} style={{ display: index < count ? 'block' : 'none' }}>
+                    {(() => {
+                        switch (ele.type) {
+                            case 'select':
+                                return (
+                                    <Form.Item name={`${ele.value}`}
+                                        label={`${ele.name}`}
+                                        rules={[
+                                            {
+                                                required: ele.disable,
+                                                message: '请选择',
+                                            },
+                                        ]}>
+                                        <Select placeholder={`请选择${ele.name}`}>
+                                            {ele.data?.map((item: any) => {
+                                                return (
+                                                    <Option value={`${ele.value}`} key={item.value}>
+                                                        {item.name}
+                                                    </Option>
+                                                );
+                                            })}
+                                        </Select>
+                                    </Form.Item>
+                                );
+
+                            default:
+                                return (
+                                    <Form.Item name={`${ele.value}`}
+                                        label={`${ele.name}`}
+                                        rules={[
+                                            {
+                                                required: ele.disable,
+                                                message: '请输入',
+                                            },
+                                        ]}>
+                                        <Input placeholder={`${ele.name}`} />
+                                    </Form.Item>
+                                );
+                        }
+                    })()}
+                </Col>,
+            );
+        });
+        const checkEle = (
+            <Col key="check" span={24}>
+                <span>状态： </span>
+                <>
+                    <Checkbox
+                        // indeterminate={false}
+                        onChange={(e: any) => onCheckAllChange(e)}
+                        checked={checkAll}
+                    >
+                        全部
+                    </Checkbox>
+                    <Checkbox.Group
+                        options={checkBoxOptions}
+                        value={formatSearchStatus}
+                        onChange={(e: any) => SetFormatSearchStatus(e)}
+                    />
+                </>
+            </Col>
+        );
+        !hideState && children.push(checkEle);
+        return children;
+    };
+
+    const handleSearchs = (e, form) => {
+        e.preventDefault();
+        form.validateFields((err, values) => {
+            if (formatSearchStatus?.length) {
+                values.statusList = formatSearchStatus;
+            }
+            // console.log('Received values of form: ', values);
+            typeof handleSearch !== 'undefined' && handleSearch(values);
+            SetSearchParams(values);
+        });
+    };
+
+    const handleReset = (form: any) => {
+        form.resetFields();
+        SetFormatSearchStatus([]);
+        SetcheckAll(false);
+        SetSearchParams({});
+    };
+
+    const pagingSizeChanges = (size: any) => {
+        typeof pagingSizeChange !== 'undefined' && pagingSizeChange(size);
+    };
+
+    const toggle = () => {
+        SetExpand(!expand);
+    };
+
+    const SearchForm = ((props: any) => {
+        const { form, hideSearchType } = props;
+        return !hideSearchType && searchArray ? (
+            <Form layout={'inline'}>
+                <Row gutter={24}>{getFields(form)}</Row>
+                <Row>
+                    <Col span={24} style={{ textAlign: 'right' }}>
+                        <Button type="primary" onClick={(e: any) => handleSearchs(e, form)}>
+                            搜索
+                        </Button>
+                        <Button style={{ marginLeft: 8 }} onClick={() => handleReset(form)}>
+                            重置
+                        </Button>
+                        {searchArray?.length > 6 ? (
+                            expand ? (
+                                <a style={{ marginLeft: 8, fontSize: 12 }} onClick={() => toggle()}>
+                                    收起 <DownOutlined />
+                                </a>
+                            ) : (
+                                <a style={{ marginLeft: 8, fontSize: 12 }} onClick={() => toggle()}>
+                                    展开 <UpOutlined />
+                                </a>
+                            )
+                        ) : null}
+                    </Col>
+                </Row>
+            </Form>
+        ) : null;
+    });
+    return (
+        <>
+            <Card bordered={false}>
+                <h3>{tableName}</h3>
+                {radioList?.map((item: any, index) => (
+                            <div key={index}>{item}</div>
+                        ))}
+                {!hideSearch && searchArray ? (
+                    <div style={{ margin: '20px 0px' }}>
+                        <SearchForm hideSearchType={hideSearch} ref={formEl} />
+                    </div>
+                ) : null}
+                <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                    {/* {tableName ? <h3 style={{ marginBottom: '24px' }}>{tableName}</h3> : null} */}
+                    <>
+                        {actionList?.map((item: any, index) => (
+                            <div key={index}>{item}</div>
+                        ))}
+                    </>
+                </div>
+                <Table
+                    showHeader={showHeader}
+                    rowSelection={
+                        showSelection
+                            ? {
+                                selectedRowKeys: selectedRowKeys || [],
+                                onChange: (rowKeys, selectedRows) => {
+                                    handleSelectedChange(rowKeys, selectedRows);
+                                },
+                            }
+                            : undefined
+                    }
+                    loading={loading}
+                    rowKey={(_, index) => `${index}`}
+                    columns={columns}
+                    dataSource={dataSource}
+                    pagination={{
+                        showSizeChanger: true,
+                        showQuickJumper: true,
+                        size: 'small',
+                        onChange: (page: any) => {
+                            pagingChange(page);
+                        },
+                        onShowSizeChange: (page: any, size: any) => {
+                            pagingSizeChanges(size);
+                        },
+                        total: totalSize,
+                        showTotal: totalSize => `共${totalSize}个`,
+                        // pageSize: 10,
+                    }}
+                    scroll={{ x: 1300 }}
+                />
+            </Card>
+        </>
+    );
+}
--- myapp/src/pages/Zjlb/index.tsx
+++ myapp/src/pages/Zjlb/index.tsx
@@ -1,8 +1,197 @@
 // 证据列表
 import React, { useState, useEffect } from 'react';
+import BaseTable from '@/components/BaseTable'
+import{Button} from 'antd'
+
+import styles from './index.less';
 
 export default () => {
+    const [tenantList, setTenantList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [totalSize, setTotalSize] = useState(0);
+    const [pageSize, setpageSize] = useState(10);
+    const [searchWord, setSearchWord] = useState<any>({
+        tenantId: '',
+        tenantCode: '',
+        tenantName: '',
+    });
+    const columns = [
+        {
+            width: 100,
+            title: '租户Id',
+            dataIndex: 'tenantId',
+        },
+
+        {
+            title: '租户code',
+            dataIndex: 'tenantCode',
+        },
+        {
+            title: '租户名称',
+            dataIndex: 'tenantName',
+        },
+        {
+            title: '管理员',
+
+            key: 'owner',
+            dataIndex: 'owner',
+            render: owner => (
+                <div style={{ maxWidth: 200 }}>
+                    123
+                </div>
+            ),
+        },
+        {
+            title: '修改信息',
+
+            render: ({ gmtModified, modifier }) => (
+                <div style={{ maxWidth: 200 }}>
+                    123
+                </div>
+            ),
+        },
+        {
+            title: '创建信息',
+
+            render: ({ gmtCreate, creator }) => (
+                <div style={{ maxWidth: 200 }}>
+                    <div style={{ marginBottom: '10px' }}>{formatUserInfo(creator)}</div>
+                    <div style={{ fontSize: '12px', color: '#999' }}>
+                        {gmtCreate && moment(gmtCreate).format('YYYY-MM-DD HH:mm:ss')}
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: '操作',
+            key: 'actionList',
+            // fixed: 'right',
+            width: 230,
+            render: (data: any, { primaryId }) => (
+                <div
+                    style={{
+                        display: 'flex',
+                        minWidth: '150px',
+                        // justifyContent: "space-between",
+                        alignItems: 'center',
+                        lineHeight: '0px',
+                    }}
+                >
+                    <a
+                        style={{ marginRight: 30 }}
+                        onClick={() => {
+                            window.open(`./create?primaryId=${primaryId}&action=VIEW`);
+                        }}
+                    >
+                        查看
+                    </a>
+                    <a
+                        style={{ marginRight: 30 }}
+                        onClick={() => {
+                            window.open(`./create?primaryId=${primaryId}&action=UPDATED`);
+                        }}
+                    >
+                        编辑
+                    </a>
+
+                    {/* <a
+              style={{ marginRight: 30 }}
+              onClick={() => {
+                confirm({
+                  title: '你确定要删除该租户吗?',
+                  cancelText: '取消',
+                  okText: '确定',
+                  onOk: async () => {
+                    const res = await fetch({
+                      url: '/api/sophon/tenantDelete',
+                      param: {
+                        primaryId,
+                      },
+                    });
+                    if (res.success) {
+                      message.success('删除成功');
+                      getList(1, searchWord, pageSize);
+                    }
+                  },
+                });
+              }}
+            >
+              删除
+            </a> */}
+                </div>
+            ),
+        },
+    ];
+
+    useEffect(() => {
+        getList();
+    }, []);
+
+    const getList = async (
+        pageNum = 1,
+        param = { tenantId: '', tenantCode: '', tenantName: '' },
+        pageSize = 10,
+    ) => {
+        setLoading(true);
+        //   const res = await fetch({
+        //     url: '/api/sophon/tenantList',
+        //     param: { pageNum, pageSize, param },
+        //   });
+        //   if (res.success) {
+        //     setTenantList(res?.data?.tenantDTOS || []);
+        //     setTotalSize(res?.data?.count || 0);
+        //   } else {
+        //     message.error(`${res?.data}`);
+        //   }
+        setLoading(false);
+    };
+
+    const handleSearch = (data: any) => {
+        setLoading(true);
+        getList(1, data);
+    };
+
+    const handlePaging = (data: any) => {
+        setLoading(true);
+        getList(data, searchWord, pageSize);
+    };
+
+    const pagingSizeChange = (size: any) => {
+        setLoading(true);
+        setpageSize(size);
+        getList(1, searchWord, size);
+    };
+    const actionList = [
+        <Button
+          type="primary"
+          onClick={() => {
+            window.open('./create');
+          }}
+        >
+          新建
+        </Button>,
+      ];
+    const searchArray = [
+        { name: '租户id', value: 'tenantId' },
+        { name: '租户code', value: 'tenantCode' },
+        { name: '租户name', value: 'tenantName' },
+    ];
     return (
-        <>证据列表</>
+        <>
+            <BaseTable
+                dataSource={tenantList || []}
+                actionList={actionList}
+                radioList={actionList}
+                searchArray={searchArray}
+                hideState={true}
+                loading={loading}
+                columns={columns as any}
+                tableName="租户列表"
+                handleSearch={(e: any) => handleSearch(e)}
+                handlePaging={(e: any) => handlePaging(e)}
+                pagingSizeChange={(e: any) => pagingSizeChange(e)}
+                totalSize={totalSize}
+            />
+        </>
     )
 }
